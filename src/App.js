48489c8775ff78115efaed1adfeb31041007d8e3
jordengerovac <jordengerovac@gmail.com>
2022-04-02 11:17:53 -0400

    initial commit of mongodb implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { CustomProvider } from 'rsuite';
 import { PlayersTableDashboard } from './components/PlayersTableDashboard';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <CustomProvider theme="dark">
-      <div className="App">
-        <PlayersTableDashboard />
-      </div>
-    </CustomProvider>
+    <GlobalProvider>
+      <CustomProvider theme="dark">
+        <div className="App">
+          <PlayersTableDashboard />
+        </div>
+      </CustomProvider>
+    </GlobalProvider>
   );
 }
 
