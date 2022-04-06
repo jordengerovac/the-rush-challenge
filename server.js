5c4c335302e6c01fea5332bbedf4fcf665ca04b5
jordengerovac <jordengerovac@gmail.com>
2022-04-05 22:00:59 -0400

    formatting

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/playerstats', playerstats);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
